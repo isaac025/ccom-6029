296b2fc4eeb0073228ed0ab4f8cc623f45528042
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2024-08-15 09:54:10 -0400

    finished tutorial 9

--- SMoL/the-heap.rkt
+++ SMoL/the-heap.rkt
@@ -1,6 +1,10 @@
-#lang stacker/smol/hof
+#lang stacker/smol/state
 
-(deffun (add1 n)
-  (+ n 1))
-(defvar v (mvec add1))
-((vec-ref v 0) 2)
+(defvar x (mvec 1 0 2))
+(vec-set! x 1 x)
+(vlen x)
+
+(defvar v (mvec 1 2 3))
+(defvar vv (mpair v v))
+(vec-set! (right vv) 0 6)
+(left vv)
